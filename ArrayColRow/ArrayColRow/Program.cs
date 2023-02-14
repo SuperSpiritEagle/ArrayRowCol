6bc65ba0f6f50143ff96b146a1f075c1cbc2b60d
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-02-14 15:25:14 +0500

    init

---
+++ ArrayColRow/ArrayColRow/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ArrayColRow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 3;
+            int column = 3;
+            int[,] array = new int[row, column];
+            int numberRow = 1;
+            int numberColumn = 0;
+            int sum = 0;
+            int composition = 1;
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(0, 9);
+
+                    if (i == numberRow)
+                    {
+                        sum += array[i, j];
+                    }
+
+                    if (j == numberColumn)
+                    {
+                        composition *= array[i, j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Сумма втрой строки = {sum}");
+            Console.WriteLine($"Произведение первого столбца = {composition}");
+        }
+    }
+}
