bf1a801f5b643aee0098c07c5fb60ece51be34e1
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-02-17 18:58:13 +0500

    GetLength(0)

--- ArrayColRow/ArrayColRow/Program.cs
+++ ArrayColRow/ArrayColRow/Program.cs
@@ -9,12 +9,14 @@ namespace ArrayColRow
             int row = 3;
             int column = 3;
             int[,] array = new int[row, column];
-            int numberRow = 1;
-            int numberColumn = 0;
-            int minNumber = 0;
-            int maxNumber = 9;
+
+
+            int minNumber = 1;
+            int maxNumber = 10;
             int sum = 0;
             int composition = 1;
+            int numberRow = 1;
+            int numberColumn = 0;
 
             Random random = new Random();
 
@@ -30,31 +32,19 @@ namespace ArrayColRow
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    if (i == numberRow)
-                    {
-                        sum += array[i, j];
-                    }
-                }
-            }
-
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    if (j == numberColumn)
-                    {
-                        composition *= array[i, j];
-                    }
-                }
-            }
-
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
                     Console.Write(array[i, j] + " ");
                 }
                 Console.WriteLine();
+            }
+
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                sum += array[numberRow, j];
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                composition *= array[i, numberColumn];
             }
 
             Console.WriteLine($"Сумма втрой строки = {sum}");
