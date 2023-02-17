394f625f6a2834253b7abcb749de8cfd9fee7afe
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-02-17 10:43:47 +0500

    for

--- ArrayColRow/ArrayColRow/Program.cs
+++ ArrayColRow/ArrayColRow/Program.cs
@@ -11,22 +11,30 @@ namespace ArrayColRow
             int[,] array = new int[row, column];
             int numberRow = 1;
             int numberColumn = 0;
+            int minNumber = 0;
+            int maxNumber = 9;
             int sum = 0;
             int composition = 1;
 
             Random random = new Random();
 
-            for (int i = 0; i < array.GetLength(1); i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    array[i, j] = random.Next(0, 9);
+                    array[i, j] = random.Next(minNumber, maxNumber);
 
                     if (i == numberRow)
                     {
                         sum += array[i, j];
                     }
+                }
+            }
 
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
                     if (j == numberColumn)
                     {
                         composition *= array[i, j];
